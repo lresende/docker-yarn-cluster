0b30330b5272dcfc6ab9e17d7c2894dcf85a0c0f
Luciano Resende <lresende@apache.org>
2015-01-16 18:17:19 -0800

    Add additional parameters to control image startup
    New supported startup parameters
      —namenode: start name node related services
      —datanode: start only datanode related services

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,18 +9,21 @@ rm /tmp/*.pid
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
 cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
-# altering the core-site configuration
-sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
-
-
 service sshd start
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
 
-if [[ $1 == "-d" ]]; then
+if [[ $1 = "-namenode" || $2 = "-namenode" ]]; then
+  $HADOOP_PREFIX/sbin/start-dfs.sh
+  $HADOOP_PREFIX/sbin/start-yarn.sh
+fi
+
+if [[ $1 = "-datanode" || $2 = "-datanode" ]]; then
+  $HADOOP_PREFIX/sbin/start-dfs.sh
+fi
+
+if [[ $1 = "-d" || $2 = "-d" ]]; then
   while true; do sleep 1000; done
 fi
 
-if [[ $1 == "-bash" ]]; then
+if [[ $1 = "-bash" || $2 = "-bash" ]]; then
   /bin/bash
 fi
